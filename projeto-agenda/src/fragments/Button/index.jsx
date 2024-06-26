62732179c05d006d8c5a40a68799fc10b1edebbb
Mxt@DEV <maxta.gamer@gmail.com>
2024-06-25 22:42:26 -0300

    update

--- projeto-agenda/src/fragments/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss';
-
-export const Button = ({ type, text }) => {
-  return (
-    <button className={styles.buttonBox} type={type}>
-      {text}
-    </button>
-  );
-};
